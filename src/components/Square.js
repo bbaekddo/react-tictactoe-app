447b277c7360e28b4c34c6b71eeafef3ea1f092a
bbaekddo <bbaekddo100@naver.com>
2023-10-03 15:50:13 +0900

    🚧 게임 보드 props 추가

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './Square.css';
 
 export class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value,
+        };
+    }
+
     render() {
+        const currentValue = this.state.value === 'X' ? 'O' : 'X';
         return (
-            <button className='square'>
-                Square Components
+            <button className='square' onClick={() => {
+                this.setState({ value: currentValue })
+            }}>
+                {this.state.value}
             </button>
         );
     }
