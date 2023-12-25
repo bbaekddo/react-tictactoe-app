646a086da1a8ee885cf0f37df8282b207145bda9
bbaekddo <bbaekddo100@naver.com>
2023-12-25 17:12:45 +0900

    🚧 승자 결정

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,9 +6,13 @@ const Board = () => {
     // getter, setter
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [isNext, setIsNext] = useState(true);
-    const status = `Next player: ${isNext ? 'X' : 'O'}`;
+    let status = `Next player: ${isNext ? 'X' : 'O'}`;
+    let winner;
 
     const handleClick = (i) => {
+        // 종료 조건
+        if (winner || squares[i]) return;
+
         const clickSquares = squares.slice();
         clickSquares[i] = isNext ? 'X' : 'O';
         setIsNext(!isNext);
@@ -18,6 +22,33 @@ const Board = () => {
     const renderSquare = (i) =>  {
         return <Square value={squares[i]} onClick={() => handleClick(i)} />;
     };
+
+    const calculateWinner = (squares) => {
+        const winLines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < winLines.length; i++) {
+            const [a, b, c] = winLines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    };
+
+    // 우승자 확인
+    winner = calculateWinner(squares);
+    if (winner) {
+        status = `Winner : ${winner}`;
+    }
 
     return (
         <div>
